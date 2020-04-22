a4babcbf524fe5a495dc155b8c3073feb18c6d4f
pasinduwijethunga <pasinduwijethunga98@gmail.com>
2020-04-22 15:22:40 +0530

    database and tabale create

---
+++ app/src/main/java/com/example/salesmart/DataBase/DBHandler.java
@@ -0,0 +1,36 @@
+package com.example.salesmart.DataBase;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHandler extends SQLiteOpenHelper {
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Product.db";
+
+    public DBHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + Product.items.TABLE_NAME + " (" +
+                    Product.items._ID + " INTEGER PRIMARY KEY," +
+                    Product.items.COLUMN_1+ " TEXT," +
+                    Product.items.COLUMN_2 + " TEXT)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + Product.items.TABLE_NAME;
+}
