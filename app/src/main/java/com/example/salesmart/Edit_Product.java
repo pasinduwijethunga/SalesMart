bf3ac2b27875a14f9f4e031d5488f02576fcaac1
pasinduwijethunga <pasinduwijethunga98@gmail.com>
2020-04-22 19:10:49 +0530

    update edit java

--- app/src/main/java/com/example/salesmart/Edit_Product.java
+++ app/src/main/java/com/example/salesmart/Edit_Product.java
@@ -7,11 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.salesmart.DataBase.DBHandler;
+
+import java.util.List;
 
 public class Edit_Product extends AppCompatActivity {
 
     EditText name,descrip,status,price;
-    Button update,delete,cancel;
+    Button update,delete,cancel,search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +30,44 @@ public class Edit_Product extends AppCompatActivity {
         update= findViewById(R.id.btnUPEP);
         delete= findViewById(R.id.btnDeleteEP);
         cancel= findViewById(R.id.btnCnclEP);
+        search= findViewById(R.id.buttonsearchEP);
+
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHandler dbHandler = new DBHandler(getApplicationContext());
+                List user = dbHandler.readAllInfo(name.getText().toString());
+                if (user.isEmpty()){
+                    Toast.makeText(Edit_Product.this, "No Product", Toast.LENGTH_SHORT).show();
+                    name.setText(null);
+                }
+                else {
+                    Toast.makeText(Edit_Product.this, "Foud Product"+user.get(0).toString(), Toast.LENGTH_SHORT).show();
+                    name.setText(user.get(0).toString());
+                    descrip.setText(user.get(1).toString());
+                    status.setText(user.get(2).toString());
+                    price.setText(user.get(3).toString());
+                }
+
+
+
+
+            }
+        });
 
         update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),Admin_View.class);
-                startActivity(i);
+                DBHandler dbHandler = new DBHandler(getApplicationContext());
+                Boolean Status = dbHandler.updateInfo(name.getText().toString(),descrip.getText().toString(),status.getText().toString(),price.getText().toString());
+                if (Status) {
+                    Toast.makeText(Edit_Product.this, "Product Updated", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(Edit_Product.this, " Updated Faild", Toast.LENGTH_SHORT).show();
+                };
+
 
             }
         });
