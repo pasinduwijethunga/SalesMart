00c9928516dc5ca02c0b15a99a2f4a93b8d18c31
pasinduwijethunga <pasinduwijethunga98@gmail.com>
2020-04-22 17:34:40 +0530

    update edit java file

--- app/src/main/java/com/example/salesmart/Edit_Product.java
+++ app/src/main/java/com/example/salesmart/Edit_Product.java
@@ -3,8 +3,13 @@ package com.example.salesmart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Edit_Product extends AppCompatActivity {
+
+    EditText name,descrip,status,price;
+    Button update,delete,cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
