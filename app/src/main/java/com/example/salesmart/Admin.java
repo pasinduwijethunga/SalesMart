a2ae3147bad5674f2a32944efb9536fb030a22a3
pasinduwijethunga <pasinduwijethunga98@gmail.com>
2020-04-22 17:12:48 +0530

    add onliclick function for view button

--- app/src/main/java/com/example/salesmart/Admin.java
+++ app/src/main/java/com/example/salesmart/Admin.java
@@ -18,11 +18,22 @@ public class Admin extends AppCompatActivity {
         add = findViewById(R.id.buttonadd);
         view = findViewById(R.id.buttonview);
 
+
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(),Add_Product.class);
                 startActivity(i);
+            }
+
+        });
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Admin_View.class);
+                startActivity(i);
+
             }
         });
     }
