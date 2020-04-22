6c5934a1224816d726f3030c40be889d7c905c00
pasinduwijethunga <pasinduwijethunga98@gmail.com>
2020-04-22 16:13:25 +0530

    ReadAllInfo function

--- app/src/main/java/com/example/salesmart/DataBase/DBHandler.java
+++ app/src/main/java/com/example/salesmart/DataBase/DBHandler.java
@@ -2,8 +2,13 @@ package com.example.salesmart.DataBase;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBHandler extends SQLiteOpenHelper {
     // If you change the database schema, you must increment the database version.
@@ -92,5 +97,45 @@ public class DBHandler extends SQLiteOpenHelper {
         // Issue SQL statement.
         int deletedRows = db.delete(Product.items.TABLE_NAME, selection, selectionArgs);
 
+    }
+
+    public List readAllInfo(){
+        String name = "Asvinash";
+        SQLiteDatabase db = getReadableDatabase();
+
+// Define a projection that specifies which columns from the database
+// you will actually use after this query.
+        String[] projection = {
+                BaseColumns._ID,
+                Product.items.COLUMN_1,
+                Product.items.COLUMN_2,
+                Product.items.COLUMN_3,
+                Product.items.COLUMN_4
+        };
+
+// Filter results WHERE "title" = 'My Title'
+        String selection = Product.items.COLUMN_1 + " = ?";
+        String[] selectionArgs = { name};
+
+// How you want the results sorted in the resulting Cursor
+        String sortOrder = Product.items.COLUMN_1 + " ASC";
+
+        Cursor cursor = db.query(
+                Product.items.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                 null,              // The columns for the WHERE clause
+                 null,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+        );
+
+        List pname = new ArrayList<>();
+        while(cursor.moveToNext()) {
+            String  iname = cursor.getString(cursor.getColumnIndexOrThrow(Product.items.COLUMN_1));
+           pname.add(iname );
+        }
+     cursor.close();
+        return pname;
     }
 }
