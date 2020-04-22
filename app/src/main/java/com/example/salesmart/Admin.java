b20802f0cc44348003dc50a5634545d36e4c795f
pasinduwijethunga <pasinduwijethunga98@gmail.com>
2020-04-22 17:08:59 +0530

    add onliclick function for add button

--- app/src/main/java/com/example/salesmart/Admin.java
+++ app/src/main/java/com/example/salesmart/Admin.java
@@ -2,13 +2,28 @@ package com.example.salesmart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Admin extends AppCompatActivity {
 
+    Button add,view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        add = findViewById(R.id.buttonadd);
+        view = findViewById(R.id.buttonview);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Add_Product.class);
+                startActivity(i);
+            }
+        });
     }
 }
