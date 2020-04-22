87ccf246b90a31d64d13693c32b8144ebd54990a
pasinduwijethunga <pasinduwijethunga98@gmail.com>
2020-04-22 17:43:47 +0530

    edit java file updated

--- app/src/main/java/com/example/salesmart/Edit_Product.java
+++ app/src/main/java/com/example/salesmart/Edit_Product.java
@@ -2,7 +2,9 @@ package com.example.salesmart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,5 +17,32 @@ public class Edit_Product extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit__product);
+
+        name = findViewById(R.id.editTextNameEP);
+       descrip = findViewById(R.id.editTextDescripEP);
+        status = findViewById(R.id.editTextStsEP);
+        price = findViewById(R.id.editTextPriceEP);
+        update= findViewById(R.id.btnUPEP);
+        delete= findViewById(R.id.btnDeleteEP);
+        cancel= findViewById(R.id.btnCnclEP);
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Admin_View.class);
+                startActivity(i);
+
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Admin_View.class);
+                startActivity(i);
+
+            }
+        });
+
     }
 }
