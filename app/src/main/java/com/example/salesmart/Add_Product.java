08f894607883779b06638fdf757980002e7a49d3
pasinduwijethunga <pasinduwijethunga98@gmail.com>
2020-04-24 18:31:12 +0530

    add onliclick function for add button

--- app/src/main/java/com/example/salesmart/Add_Product.java
+++ app/src/main/java/com/example/salesmart/Add_Product.java
@@ -10,8 +10,13 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.salesmart.DataBase.DBHandler;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Add_Product extends AppCompatActivity {
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference reference;
 
     EditText name,descrip,status,price;
     Button add,cancel;
@@ -33,17 +38,24 @@ public class Add_Product extends AppCompatActivity {
             public void onClick(View v) {
 
 
-                DBHandler dbHandler = new DBHandler(getApplicationContext());
-                long newID=     dbHandler.addInfo(name.getText().toString(),descrip.getText().toString(),status.getText().toString(),price.getText().toString());
-                Toast.makeText(Add_Product.this,"Product Added. Product ID"+newID,Toast.LENGTH_SHORT).show();
+                //DBHandler dbHandler = new DBHandler(getApplicationContext());
+                //long newID=     dbHandler.addInfo(name.getText().toString(),descrip.getText().toString(),status.getText().toString(),price.getText().toString());
+                //Toast.makeText(Add_Product.this,"Product Added. Product ID"+newID,Toast.LENGTH_SHORT).show();
 
                 Intent i = new Intent(getApplicationContext(),List_view.class);
                 startActivity(i);
 
-                name.setText(null);
-                descrip.setText(null);
-                status.setText(null);
-                price.setText(null);
+                //name.setText(null);
+                //descrip.setText(null);
+                //status.setText(null);
+                //price.setText(null);
+                String prName = name.getText().toString().trim();
+                String prDescription = descrip.getText().toString().trim();
+                String prStatus = status.getText().toString().trim();
+                String prPrice = price.getText().toString().trim();
+
+                firebaseDatabase = FirebaseDatabase.getInstance();
+                reference = firebaseDatabase.getReference();
 
 
 
