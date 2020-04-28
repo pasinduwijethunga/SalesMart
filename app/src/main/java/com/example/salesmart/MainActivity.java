9ad5058346135bba279dc5b844eb500e5bea5d01
pasinduwijethunga <pasinduwijethunga98@gmail.com>
2020-04-28 10:28:14 +0530

    commited

---
+++ app/src/main/java/com/example/salesmart/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.salesmart;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference reference;
+    RecyclerView recyclerView;
+    ArrayList<Product> list;
+    Product_Adapter product_adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.myrecyler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        list = new ArrayList<Product>();
+
+
+        reference = FirebaseDatabase.getInstance().getReference().child("products");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot dataSnapshot1: dataSnapshot.getChildren()){
+
+                    Product p = dataSnapshot1.getValue(Product.class);
+                    list.add(p);
+
+                }
+                product_adapter = new Product_Adapter(MainActivity.this,list);
+                recyclerView.setAdapter(product_adapter);
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+}
