899ab649651699d89f1dddf890960d29abb4c36d
pasinduwijethunga <pasinduwijethunga98@gmail.com>
2020-04-30 14:36:03 +0530

    commited

---
+++ app/src/main/java/com/example/salesmart/Admin_Panel.java
@@ -0,0 +1,14 @@
+package com.example.salesmart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Admin_Panel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin__panel);
+    }
+}
